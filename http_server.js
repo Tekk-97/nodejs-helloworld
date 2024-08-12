45e239d20c10e71a63bb1bdaf8491bf5fe3049b5
Tekk <59083912+Tekk-97@users.noreply.github.com>
2024-08-12 10:36:36 +0900

    Update http_server.js

--- http_server.js
+++ http_server.js
@@ -1,6 +1,6 @@
 var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
+  res.end('240812 10:35 CI/CD is work\n');
 }).listen(3000, "0.0.0.0");
 console.log('Server running at your public IP:3000');
